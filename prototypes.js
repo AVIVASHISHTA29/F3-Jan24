5ad614a42d69525df7d718c193afb95c31ff8a48
Avi Vashishta <avivashishta@gmail.com>
2024-01-10 11:02:29 -0500

    prototypes and prototypal chain

--- prototypes.js
+++ prototypes.js
@@ -1,20 +1,62 @@
-// let arr = [1, 2, 3];
+let arr1 = [1, 2, 3];
 // console.log(arr);
-let arr = new Array(1, 2, 3);
+// let arr1 = new Array(1, 2, 3);
 
-Array.prototype.getSum = function () {
-    let sum = 0;
-    this.forEach((item, i) => {
-        sum += item
-    })
-    return sum;
+// Array.prototype.getSum = function () {
+//     let sum = 0;
+//     this.forEach((item, i) => {
+//         sum += item
+//     })
+//     return sum;
+// }
+
+// let arr2 = [4, 5, 6, 7, 8, 9, 10];
+
+
+// console.log(arr2.getSum());
+
+// let name = "AVi";
+
+// String.prototype.getFullName = function () {
+//     return `My Name is ${this}`
+// }
+
+
+// let arr3 = [10, 10, 329823, 1298, 209283, 1029812, 1288713];
+// console.log(arr3.map2())
+
+// Array.prototype.map = function () {
+//     let newArr = this.filter((item) => item % 2 == 0);
+//     return newArr
+// }
+// console.log(arr1);
+// console.log(arr1.map());
+
+// console.log(arr.getSum());
+
+// console.log(arr2.getSum());
+
+// let obj = new Object();
+// obj.name = "Avi";
+// obj.email = "avi@gmail.com";
+
+// Object.prototype.getNameAndEmail = function () {
+//     return `My Name is ${this.name} and Email is ${this.email}`
+// }
+
+// console.log(obj);
+
+// Object.prototype.abc = function () {
+//     return "abc"
+// }
+
+
+let values = {
+    a: 10,
+    b: 20,
+    c: 30
 }
 
-let arr2 = [4, 5, 6, 7, 8, 9, 10]
-
-console.log(arr.getSum());
-
-console.log(arr2.getSum());
 
 let functionsObj = {
     getName: function () {
@@ -28,7 +70,8 @@ let functionsObj = {
     },
     toString: function () {
         return JSON.stringify(this);
-    }
+    },
+    __proto__: values
 }
 
 
@@ -51,3 +94,26 @@ let obj3 = {
 }
 
 console.log(obj1)
+
+
+let grandfather = {
+    house: "Grand father house"
+}
+
+
+let myFather = {
+    car: "suzuki",
+    house: "father new house",
+    __proto__: grandfather
+}
+
+
+let me = {
+    __proto__: myFather,
+    // house: myFather.__proto__.house,
+}
+
+
+console.log(grandfather.house);
+console.log(myFather.house);
+console.log(me.house);
